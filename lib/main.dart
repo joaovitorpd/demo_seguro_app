c8399eec70d29e2353f4a510ceab1f17ba9d90d5
joaovitorpd <joaovitorpd@gmail.com>
2025-11-18 13:54:31 -0300

    feat: integrate Firebase App Check and update dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:demo_seguro_app/app/modules/auth/providers/auth_provider.dart';
 import 'package:demo_seguro_app/app/utils/platform_other_setup.dart';
 import 'package:demo_seguro_app/firebase_options.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -10,12 +12,26 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   initializePlatform();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: kDebugMode
+        ? AndroidProvider.debug
+        : AndroidProvider.playIntegrity,
+    appleProvider: kDebugMode ? AppleProvider.debug : AppleProvider.appAttest,
+    webProvider: kDebugMode
+        ? ReCaptchaV3Provider(
+            'a60e93b6-e30f-4497-8166-5793effde1c9',
+          ) // Chave DEBUG
+        : ReCaptchaV3Provider(
+            '6Le0LnQrAAAAAKimXiDH9lwRd88utqkQYAhpyi2k',
+          ), // Chave PROD
+  );
 
   final storage = const FlutterSecureStorage(
     aOptions: AndroidOptions(encryptedSharedPreferences: true),
